0a5b64941fe421277a3eb293c2df4d42ec9a2677
dladden <dladden77@gmail.com>
2023-06-02 08:41:45 -0400

    first commit

---
+++ nextjs.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+module.exports = {
+  //trailingSlash for seo adds trailing slash after every url
+  trailingSlash: false,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+
+    return config;
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
